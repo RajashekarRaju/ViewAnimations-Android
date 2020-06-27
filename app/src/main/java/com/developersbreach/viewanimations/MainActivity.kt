e822ed192533f818f3deb56f3d4ae03b541704fa
Rajasekhar <rajashekarraju123@gmail.com>
2020-06-27 19:49:40 +0530

    Added CardView to apply transitions and animations

--- app/src/main/java/com/developersbreach/viewanimations/MainActivity.kt
+++ app/src/main/java/com/developersbreach/viewanimations/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.developersbreach.viewanimations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardView = findViewById<MaterialCardView>(R.id.card_view_animate)
+        cardView.setOnClickListener {
+            startCircularFillingEffect(cardView, applicationContext)
+        }
     }
 }
