24d592ddbbc1c27f30e277b1488b46488dfe5e03
Rajasekhar <rajashekarraju123@gmail.com>
2020-06-29 04:48:21 +0530

    setup NavigationUI with ActionBar and Controller for up button fix, changed styles.xml

--- app/src/main/java/com/developersbreach/viewanimations/MainActivity.kt
+++ app/src/main/java/com/developersbreach/viewanimations/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
