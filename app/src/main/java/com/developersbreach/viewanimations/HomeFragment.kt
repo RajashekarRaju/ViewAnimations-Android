17720b453fe119f23dbea9641a195a69cf98f890
Rajasekhar <rajashekarraju123@gmail.com>
2020-06-30 23:57:48 +0530

    Added new destination fragment to discover other fragments

---
+++ app/src/main/java/com/developersbreach/viewanimations/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.developersbreach.viewanimations
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.simple_transition_text_view).setOnClickListener {
+            findNavController().navigate(R.id.homeToSimpleTransitionsFragment)
+        }
+
+        view.findViewById<TextView>(R.id.coordinated_motion_text_view).setOnClickListener {
+            findNavController().navigate(R.id.HomeToCoordinatedMotionFragment)
+        }
+    }
+}
