eb4b2b6bdf7bc5d6b5b4c6e141396c4d4caef34f
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-01 17:25:50 +0530

    Replaced TextView with CardViews, added animation and gradient background to cards

--- app/src/main/java/com/developersbreach/viewanimations/HomeFragment.kt
+++ app/src/main/java/com/developersbreach/viewanimations/HomeFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import com.developersbreach.viewanimations.coorodinated.startAnimation
+import com.google.android.material.card.MaterialCardView
 
 
 class HomeFragment : Fragment() {
@@ -22,12 +25,15 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.simple_transition_text_view).setOnClickListener {
+        view.findViewById<MaterialCardView>(R.id.simple_transition_main_card_view).setOnClickListener {
             findNavController().navigate(R.id.homeToSimpleTransitionsFragment)
         }
 
-        view.findViewById<TextView>(R.id.coordinated_motion_text_view).setOnClickListener {
-            findNavController().navigate(R.id.HomeToCoordinatedMotionFragment)
+        view.findViewById<MaterialCardView>(R.id.coordinated_motion_card_view).setOnClickListener {
+            findNavController().navigate(R.id.homeToCoordinatedMotionFragment)
         }
+
+        val parent: ConstraintLayout = view.findViewById(R.id.home_fragment_parent)
+        startAnimation(android.R.interpolator.overshoot, parent, requireContext())
     }
 }
