7c451fe71e1bf29c4d5a97fa05b58862799ff911
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-07 21:39:18 +0530

    Fragment for ImageView which transforms with MaterialContainerTransform

---
+++ app/src/main/java/com/developersbreach/viewanimations/motion/MotionFragment.kt
@@ -0,0 +1,41 @@
+package com.developersbreach.viewanimations.motion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import com.developersbreach.viewanimations.R
+import com.google.android.material.transition.MaterialContainerTransform
+
+class MotionFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = MaterialContainerTransform()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_motion, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageView: ImageView = view.findViewById(R.id.motion_image_item)
+
+        imageView.setOnClickListener {
+            val extras = FragmentNavigatorExtras(
+                imageView to getString(R.string.motion_transition_name)
+            )
+            findNavController().navigate(R.id.motionToMotionDetailFragment, null, null, extras)
+        }
+    }
+}
